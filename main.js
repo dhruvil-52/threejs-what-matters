c9b9c0890ea2d2fdcf53935283a178805ce68571
Dhruvil <Dhruvil@buildesk.in>
2024-09-22 18:45:24 +0530

    OrbitControls added so that object react on cursor, add comments

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+// create scene (whole world)
 const scene = new THREE.Scene();
+// create camera (how much area we want to show || how much are we want to render into scene)
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 5;
 
+// make geometry and material then combine it to mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
@@ -16,18 +21,21 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-
-const mycanvas = document.querySelector(".canvas");
+// create renderer and attach to our document
+const mycanvas = document.getElementById("canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: mycanvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
+// control our object by using orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// animate our scene
 function animate() {
-  window.requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
 // renderer.setAnimationLoop(animate);
