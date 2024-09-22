9203d2f3cda7890acb2ecee8b9bd04a1703f8b92
Dhruvil <Dhruvil@buildesk.in>
2024-09-23 00:39:52 +0530

    implemented mesh standard material: use for make realistic objects, laso add helpers for seeing from where lights comes and our object is visible

--- main.js
+++ main.js
@@ -22,12 +22,42 @@ const camera = new THREE.PerspectiveCamera(
 // const cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
+// AxesHelper (visualizes the axes: red = X, green = Y, blue = Z)
+const axesHelper = new THREE.AxesHelper(5); // Length of the axes lines
+scene.add(axesHelper);
+
+// GridHelper (a grid plane)
+const gridHelper = new THREE.GridHelper(100, 100); // Size of grid and number of divisions
+scene.add(gridHelper);
+
+// DirectionalLightHelper (shows the direction of a directional light)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5); // High intensity light
+directionalLight.position.set(10, 20, 15); // Set the position of the light
+scene.add(directionalLight);
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  5
+); // 5 is the size of the helper
+scene.add(directionalLightHelper);
+
+// CameraHelper (visualizes the frustum of the camera)
+const cameraHelper = new THREE.CameraHelper(camera);
+scene.add(cameraHelper);
+
 // SphereGeometry
 camera.position.z = 30;
 const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true, // remove this line if you want solid object
+const material = new THREE.MeshStandardMaterial({
+  color: "blue",
+  // wireframe: true, // remove this line if you want solid object
+  // side: THREE.DoubleSide,
+  // roughness: 0.5, // 0 is smooth (like a mirror), 1 is rough,
+  // metalness: 0.8, // 0 is non-metal, 1 is fully metallic
+  // emissive: "blue", // Emits red light
+  // emissiveIntensity: 1, // Intensity of the emitted light
+  // transparent: true, // Allow transparency
+  // opacity: 0.5, // Semi-transparent
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
